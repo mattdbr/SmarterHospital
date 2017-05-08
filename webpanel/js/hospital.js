a1f5a1ea16b48abf128352b730e69e9b9cec7975
Matthew Brener <Matthew Brener>
2017-05-08 15:06:54 +1000

    jquery now works (verified with console.log)

--- webpanel/js/hospital.js
+++ webpanel/js/hospital.js
@@ -1,11 +1,12 @@
-$(function() {
+$(document).ready(function(){
 	getSensorvalue();
 });
+
 
 function getSensorvalue() {
 //Every one second, this function obtains sensor values from Arduino Yun and sends to Yun
 //a request to turn LED on or off (depending on what radio button is pressed).
-
+	console.log('this is executing');
 	var checked_option_radio = $('input[name=LEDCheck]:checked','#LED_Selection').val(); //Check what
 	//radio button is pressed ("LED on" or "LED off")
 
@@ -24,8 +25,9 @@ function getSensorvalue() {
 		$('#LED_content').load('/arduino/light/255');
 	}
 
-	$('#inBed').load('/arduino/onbed);
-	$('#heartrate').load('/arduino/heartrate);
+	$('#inBed').load('/arduino/onbed');
+	$('#heartrate').load('/arduino/heartrate');
+	
 	
 	setTimeout("getSensorvalue()",1000); //Poll every one seconds. Lower = less real time but lower power consumption
 }
