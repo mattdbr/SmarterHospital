80aa759bbb3b777c0d4f735af2d2692897e24316
Matthew Brener <akkatracker@gmail.com>
2017-05-21 14:18:53 +1000

    Add temperature detection + changing

--- webpanel/js/hospital.js
+++ webpanel/js/hospital.js
@@ -1,17 +1,35 @@
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
 $(function(){
 	//sliderInit();
 	getSensorvalue();
 });
 
-$('#submit').click(function(){
+$('#light-submit').click(function(){
     changelight();
 });
 
+$('#temp-submit').click(function(){
+    changetemp();
+});
+
+var ip_address = '149.171.143.197'; //global as we need to access it in a lot of places
+var adjusting_temp = false;
+var temp_mode = 3; // 1 = fan 2 = heat 3 = off. Used to prevent excessive API calls.
+
 function changelight(){
-	var ip_address = '149.171.143.197';		
 	var value = $('.value').text(); // get value here
 	$('#LED_content').load('http://' + ip_address + '/arduino/light/' + value);
-	console.log(value); //to test slider is working - TODO: Find way to only send if submit is pressed
+	console.log(ip_address);
+}
+
+function changetemp(){	
+	adjusting_temp = true;
+	console.log(value);
 }
 
 
@@ -35,32 +53,27 @@ function changelight(){
 function getSensorvalue() {
 //Every one second, this function obtains sensor values from Arduino Yun and sends to Yun
 //a request to turn LED on or off (depending on what radio button is pressed).
-	var ip_address = '149.171.143.197'; // store IP as a variable to change once on the day
 	var delay = 5000; // change to affect poll interval
 	
-	var checked_option_radio = $('input[name=LEDCheck]:checked','#LED_Selection').attr('id'); 
-	//Get the id of the checked radio button
-
-	if (checked_option_radio =='off') //if the user selected for LED to be off, send request to Arduino
-	{
-		$('#LED_content').load('http://' + ip_address + '/arduino/nolight');
-		console.log('nolight'); //for testing
-	}
-	
-	if (checked_option_radio =='half') //if the user selected for LED to be off, send request to Arduino
-	{
-		$('#LED_content').load('http://' + ip_address + '/arduino/halflight');
-		console.log('halflight'); //for testing
-	}
-	
-	if (checked_option_radio =='on') //if the user selected for LED to be on, send request to Arduino
-	{
-		$('#LED_content').load('http://' + ip_address + '/arduino/light/fulllight');
-		console.log('fulllight'); //for testing
-	}
-
+	$('.temperature').load('http://'+ ip_address + '/arduino/temperature');
 	$('#inBed').load('http://'+ ip_address + '/arduino/onbed'); //TODO: If not in bed, set in red. API should return "in bed" or "not in bed"
 	$('#heartrate').load('http://' + ip_address+ '/arduino/heartrate'); //TODO: If bad, set in red
+	
+	var value = $('.validate').text(); // get value here
+	var temp = $('.temperature').text();
+	if(adjusting_temp){
+		if(temp > value + 1 && temp_mode != 1){
+			temp_mode = 1;
+			$('#action').load('http://' + ip_address + '/arduino/fan' + value);
+		}else if(temp < value - 1 && temp_mode != 2){
+			temp_mode = 2;
+			$('#action').load('http://' + ip_address + '/arduino/heat');
+		}else if(temp_mode != 3){
+			$('#action').load('http://' + ip_address + '/arduino/off');
+			temp_mode = 3;
+			adjusting_temp = false;
+		}
+	}
 	
 	setTimeout("getSensorvalue()", delay); //Wait... Lower = less real time but lower power consumption. Tradeoff we've got to calculate. 
 }
