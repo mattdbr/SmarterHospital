635f0ca2d178625644fef604b413035672d15d6b
Matthew Brener <akkatracker@gmail.com>
2017-05-29 11:23:31 +1000

    Added patient profile

--- webpanel/js/hospital.js
+++ webpanel/js/hospital.js
@@ -200,3 +200,26 @@ function alarm(){
 	browserNotifications("Alarm Triggered");
 	$.get("http://" + ip_address + "/arduino/alarm");
 }
+
+function clock() {// We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+    
+    // Access the "getHours" method on the Date object with the dot accessor.
+    hours = time.getHours(),
+    
+    // Access the "getMinutes" method with the dot accessor.
+    minutes = time.getMinutes(),
+    
+    
+    seconds = time.getSeconds();
+
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+  
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);
